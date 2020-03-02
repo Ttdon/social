3a491909f18931eef6756eddd7e78c4b216c36ab
Ttdon <56308588+Ttdon@users.noreply.github.com>
2020-03-02 12:25:42 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,20 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '174836217294263', //  App ID
+        'clientSecret'  : '70a08d335847e43bb5dc9e91cb73fab4', //  App Secret
+        'callbackURL'   : '/auth/facebook/callback'
+        // 'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        // 'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'googleAuth' : {
+        'clientID'      : '836472167867-442p09lv3e7s0o5qhmnf6qejj6l116nh.apps.googleusercontent.com',
+        'clientSecret'  : '_Cg_57AuhwpWx4BI1yYZGwoF',
+        'callbackURL'   : '/auth/google/callback'
+    }
+
+};
